83c9bf6f24808c0541ec6781a486d48fe6ab788a
DIKSHA CHAMYAL <149150180+Diksha0012@users.noreply.github.com>
2024-06-17 12:34:10 +0530

    Update clock.c

--- clock.c
+++ clock.c
@@ -3,14 +3,14 @@
 
 int main(){
 	
-	time_t s,val=1;
+	time_t second,val=1;
 	struct tm* current_time;
 	
 	//time in seconds
-	s=time(NULL);
+	second=time(NULL);
 	
 	//to get current time
-	current_time = localtime(&s);
+	current_time = localtime(&second);
 	printf("%d : %d : %d",current_time->tm_hour,current_time->tm_min,current_time->tm_sec);
 	
 	return 0;
