44f47827fcf579ec3bedc98e1a82160934dbbb20
DIKSHA CHAMYAL <149150180+Diksha0012@users.noreply.github.com>
2024-06-17 12:32:23 +0530

    Add files via upload

---
+++ clock.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+	
+	time_t s,val=1;
+	struct tm* current_time;
+	
+	//time in seconds
+	s=time(NULL);
+	
+	//to get current time
+	current_time = localtime(&s);
+	printf("%d : %d : %d",current_time->tm_hour,current_time->tm_min,current_time->tm_sec);
+	
+	return 0;
+}
